6ed08609e4dfd4d34278a06eefc750154b3207ca
Kartikey Dubey <66429038+Kartikey0217@users.noreply.github.com>
2021-01-08 01:43:12 +0530

    Updated my keycode of diano  game

--- game.js
+++ game.js
@@ -1,11 +1,11 @@
-// score = 0;
-// cross = true;
+score = 0;
+cross = true;
 
-// audio = new Audio('music.mp3');
-// audiogo = new Audio('gameover.mp3');
-// setTimeout(() => {
-//     audio.play()
-// }, 1000);
+audio = new Audio('music.mp3');
+audiogo = new Audio('gameover.mp3');
+setTimeout(() => {
+    audio.play()
+}, 1000);
 document.onkeydown = function (e) {
     console.log("Key code is: ", e.keyCode)
     if (e.keyCode == 38) {
@@ -17,9 +17,26 @@ document.onkeydown = function (e) {
     }
     if (e.keyCode == 39) {
         dino = document.querySelector('.dino');
-        dino.classList.add('animateDinoright');
-        setTimeout(() => {
-            dino.classList.remove('animateDinoright')
-        }, 700);
+        dinoX = parseInt(window.getComputedStyle(dino, null).getPropertyValue('left'));
+        dino.style.left = dinoX + 112 + "px";
     }
+    if (e.keyCode == 37) {
+        dino = document.querySelector('.dino');
+        dinoX = parseInt(window.getComputedStyle(dino, null).getPropertyValue('left'));
+        dino.style.left = (dinoX - 112) + "px";
+    }
+}
+setInterval(() => {
+    dino = document.querySelector('.dino');
+    gameOver = document.querySelector('.gameOver');
+    obstacle = document.querySelector('.obstacle');
+
+    dx = parseInt(window.getComputedStyle(dino, null).getPropertyValue('left'));
+    dy = parseInt(window.getComputedStyle(dino, null).getPropertyValue('top'));
+
+    ox = parseInt(window.getComputedStyle(obstacle, null).getPropertyValue('left'));
+    oy = parseInt(window.getComputedStyle(obstacle, null).getPropertyValue('top'));
+
+    offsetX = Math.abs(dx - ox);
+    offsetY = Math.abs(dy - oy);
 }
